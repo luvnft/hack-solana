4cc17e30b337f1a30f97f569f5f4e3916021b3f2
The Wizard of Hahz <info@blkluv.org>
2024-02-05 15:40:51 -0500

    updated fund

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,8 +8,8 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create T3 App</title>
-        <meta name="description" content="Crowdlana" />
+        <title>SOLVC</title>
+        <meta name="Solana fund raising" content="Crowdlana" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
@@ -24,8 +24,8 @@ export default function Home() {
             <div className="hero-overlay bg-opacity-70"></div>
             <div className="hero-content text-center">
               <div className="max-w-md">
-                <h1 className="mb-5 text-5xl font-bold" style={{color: "white"}}>Innovate and Elevate!</h1>
-                <p className="mb-5 text-2xl" style={{color: "lightgrey"}}>The Future of Funding on Solana.</p>
+                <h1 className="mb-5 text-5xl font-bold" style={{color: "white"}}>SOLVC</h1>
+                <p className="mb-5 text-2xl" style={{color: "lightgrey"}}>The Future of Funding on Solana blockchain.</p>
                   <Link href="/create" className="btn m-2">
                       <button>Get paid right now →</button>
                   </Link>
